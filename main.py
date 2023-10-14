29a252d5a490bef6db35368303dc348f7d38ff5a
PatoPon <novaesgui2323@outlook.com>
2023-10-14 12:17:08 -0300

    Interface melhor

--- main.py
+++ main.py
@@ -1,57 +1,91 @@
-import tkinter as tk
-from tkinter import filedialog, simpledialog, Label
+import sys
 import os
 import vlc
 import yt_dlp
-from youtube_search import YoutubeSearch
-import sys
 import threading
+from youtube_search import YoutubeSearch
+from PyQt5.QtWidgets import QInputDialog, QApplication, QWidget, QLabel, QPushButton, QFileDialog, QSlider, QVBoxLayout
+from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtCore import Qt, QTimer
 
-musica = "hello.mp3"
+# Definir a aparência dos botões e rótulos
+def set_button_style(button):
+    button.setStyleSheet(
+        "QPushButton {"
+        "background-color: #2196F3;"
+        "border: none;"
+        "color: white;"
+        "padding: 10px 15px;"
+        "border-radius: 5px;"
+        "font-size: 16px;"
+        "}"
+        "QPushButton:hover {"
+        "background-color: #1976D2;"
+        "}"
+    )
+
+def set_label_style(label):
+    label.setAlignment(Qt.AlignCenter)
+    label.setFont(QFont("Arial", 14, QFont.Bold))
+
+# Inicializar a instância VLC e o reprodutor de mídia
 instancia = vlc.Instance("--no-video")
 player = instancia.media_player_new()
 
+# Inicializar a música como "hello.mp3"
+musica = "hello.mp3"
+
+# Função para tocar a música
 def tocarMusica(player, musica):
     if os.path.exists(musica):
         media = instancia.media_new(musica)
         player.set_media(media)
         player.play()
+        # Iniciar o temporizador para atualizar o slider
+        slider_timer.start(1000)  # Atualizar a cada segundo
     else:
         mensagem_erro = "O arquivo de música não foi encontrado."
-        tk.messagebox.showerror("Erro", mensagem_erro)
+        print("Erro:", mensagem_erro)
 
+# Função para parar a música
 def pararMusica(player):
     player.stop()
+    # Parar o temporizador quando a música é parada
+    slider_timer.stop()
 
+# Função para escolher uma música
 def escolherMusica():
     global musica
-    file_path = filedialog.askopenfilename(filetypes=[("Arquivos MP3", "*.mp3")])
+    file_path, _ = QFileDialog.getOpenFileName(None, "Escolher Música", "", "Arquivos MP3 (*.mp3)")
     if file_path:
         musica = file_path
 
+# Função para baixar uma música do YouTube
 def baixarMusica():
     global musica
-    termo_pesquisa = simpledialog.askstring("Digite o termo de pesquisa", "Digite o nome da música ou artista:")
+    termo_pesquisa, _ = QInputDialog.getText(None, "Digite o termo de pesquisa", "Digite o nome da música ou artista:")
     if termo_pesquisa:
-        percent_label.config(text="Iniciando pesquisa...")  # Defina um texto inicial
+        percent_label.setText("Iniciando pesquisa...")
         threading.Thread(target=procurarMusica, args=(termo_pesquisa,)).start()
 
+# Função para procurar música no YouTube
 def procurarMusica(termo_pesquisa):
     search_results = YoutubeSearch(termo_pesquisa, max_results=1)
     url = f"https://www.youtube.com{search_results.videos[0]['url_suffix']}"
     downloadMusica(url)
 
+# Função para fazer o download da música do YouTube
 def downloadMusica(url):
     def progress_callback(status):
         if status['status'] == 'downloading':
             percent = status['_percent_str']
-            percent_label.config(text=f"Baixando: {percent}")
+            percent_label.setText(f"Baixando: {percent}")
         elif status['status'] == 'finished':
-            percent_label.config(text="Iniciando conversão...")
+            percent_label.setText("Iniciando conversão...")
 
     ydl_opts = {
         'format': 'bestaudio/best',
-        'default-search': "ytsearch",
+        'default_search': "ytsearch",
         'progress_hooks': [progress_callback],
         'outtmpl': '%(uploader)s - %(title)s.%(ext)s',
         'postprocessors': [{
@@ -69,30 +103,96 @@ def downloadMusica(url):
 
         if not os.path.exists(nome_webm):
             global musica
-            percent_label.config(text="O arquivo está pronto!")
+            percent_label.setText("O arquivo está pronto!")
             musica = nome_mp3
 
-janela = tk.Tk()
-janela.title("Programa de Música")
-janela.geometry("400x200")
+# Função para atualizar o slider com base na posição da música
+def update_slider():
+    if player.is_playing():
+        # Obter a posição atual da música em milissegundos
+        position = player.get_time()
+        duration = player.get_length()
+        if duration > 0:
+            # Calcular a posição como uma porcentagem da duração
+            position_percentage = (position / duration) * 100
 
-percent_label = Label(janela, text="", font=("Helvetica", 14))
-percent_label.pack()
+            # Calcular o tempo decorrido e o tempo total
+            elapsed_time = position / 1000  # em segundos
+            total_time = duration / 1000  # em segundos
 
-play_music_button = tk.Button(janela, text="Tocar Música", state="active")
-play_music_button.config(command=lambda: tocarMusica(player, musica))
-play_music_button.pack()
+            # Formatando os tempos decorridos e totais como minutos:segundos
+            elapsed_time_str = "{:02d}:{:02d}".format(int(elapsed_time // 60), int(elapsed_time % 60))
+            total_time_str = "{:02d}:{:02d}".format(int(total_time // 60), int(total_time % 60))
 
-stop_music_button = tk.Button(janela, text="Parar Música", state="active")
-stop_music_button.config(command=lambda: pararMusica(player))
-stop_music_button.pack()
+            # Atualizar o rótulo com a duração decorrida e total
+            percent_label.setText(f"Duração: {elapsed_time_str} / {total_time_str}")
 
-botaoEscolherMusica = tk.Button(janela, text="Escolher Música", state="active")
-botaoEscolherMusica.config(command=escolherMusica)
-botaoEscolherMusica.pack()
+            # Atualizar o slider
+            music_slider.setValue(int(position_percentage))
 
-botaoBaixarMusica = tk.Button(janela, text="Baixar Música", state="active")
-botaoBaixarMusica.config(command=baixarMusica)
-botaoBaixarMusica.pack()
+def set_music_position(position):
+    if player.get_media() is not None:
+        duration = player.get_media().get_duration()
+        if duration > 0:
+            # Calcule a posição em milissegundos com base na porcentagem do slider
+            position_ms = (position / 100) * duration
+            player.set_time(int(position_ms))
 
-janela.mainloop()
+            # Se a música não estiver tocando ou estiver pausada, inicie a reprodução a partir da nova posição
+            if not player.is_playing() or player.get_state() == vlc.State.Ended:
+                player.play()
+
+# Inicialização da aplicação Qt
+app = QApplication(sys.argv)
+
+# Configuração da janela
+janela = QWidget()
+janela.setWindowTitle("Programa de Música / Youtube Downloader")
+janela.setGeometry(100, 100, 400, 300)
+janela.setWindowIcon(QIcon("icon.png"))
+
+layout = QVBoxLayout()
+
+# Rótulo para exibir o progresso
+percent_label = QLabel()
+set_label_style(percent_label)
+layout.addWidget(percent_label)
+
+# Botões para controlar a música
+play_music_button = QPushButton("Tocar Música")
+set_button_style(play_music_button)
+play_music_button.clicked.connect(lambda: tocarMusica(player, musica))
+layout.addWidget(play_music_button)
+
+stop_music_button = QPushButton("Parar Música")
+set_button_style(stop_music_button)
+stop_music_button.clicked.connect(lambda: pararMusica(player))
+layout.addWidget(stop_music_button)
+
+# Botões para escolher e baixar música
+botaoEscolherMusica = QPushButton("Escolher Música")
+set_button_style(botaoEscolherMusica)
+botaoEscolherMusica.clicked.connect(escolherMusica)
+layout.addWidget(botaoEscolherMusica)
+
+botaoBaixarMusica = QPushButton("Baixar Música")
+set_button_style(botaoBaixarMusica)
+botaoBaixarMusica.clicked.connect(baixarMusica)
+layout.addWidget(botaoBaixarMusica)
+
+# Slider para controlar a posição da música
+music_slider = QSlider(Qt.Horizontal)
+layout.addWidget(music_slider)
+
+# Criando um temporizador para atualizar o slider automaticamente
+slider_timer = QTimer()
+slider_timer.timeout.connect(update_slider)
+
+# Conectar o evento sliderMoved ao método set_music_position
+music_slider.sliderMoved.connect(set_music_position)
+
+# Configuração da janela principal
+janela.setLayout(layout)
+janela.show()
+
+sys.exit(app.exec_())
