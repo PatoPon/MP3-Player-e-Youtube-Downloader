1690b0ba09483d987949725edbb6459ddd44abf9
PatoPon <novaesgui2323@outlook.com>
2023-10-16 20:01:56 -0300

    Reestruturação do código

--- logica.py
+++ logica.py
@@ -1,16 +1,19 @@
+from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import *
-import vlc
+from PyQt5.QtCore import Qt
+import pygame  # Importe o Pygame
 import os, re, urllib.parse
 
-def tocarMusica(player, media_list):
-    print(media_list)
-    player.set_media_list(media_list)
-    player.play()
+def tocarMusica(playlist):
+    if playlist:
+        pygame.mixer.music.load(playlist[0]['path'])
+        pygame.mixer.music.play()
 
-def proximaMusica(player):
-    player.next()
+def adicionarMusicaAPlaylist(playlist, file_path):
+    nome_musica = limpar_nome_musica(os.path.basename(file_path))
+    playlist.append({'path': file_path, 'nome_musica': nome_musica})
 
-def escolherMusica(player, media_list):
+def escolherMusica(playlist):
     file_dialog = QFileDialog()
     file_dialog.setNameFilter("Arquivos de Áudio (*.mp3)")
     file_dialog.setViewMode(QFileDialog.List)
@@ -19,75 +22,25 @@ def escolherMusica(player, media_list):
     if file_dialog.exec_():
         selected_files = file_dialog.selectedFiles()
         if selected_files:
-            # Criar uma nova lista de reprodução
-            media_list = vlc.MediaList()
+            # Crie um conjunto de músicas já existentes na playlist
+            musicas_na_playlist = set(musica['nome_musica'] for musica in playlist)
 
-            # Adicionar os novos arquivos selecionados à lista de reprodução
             for file_path in selected_files:
-                media = vlc.Media(file_path)
-                media_list.add_media(media)
-
-            # Substituir a lista de reprodução existente
-            player.set_media_list(media_list)
-
-            # Tocar a primeira música da nova lista
-            player.play()
+                nome_musica = limpar_nome_musica(os.path.basename(file_path))
+                # Verifique se a música já está na playlist antes de adicioná-la
+                if nome_musica not in musicas_na_playlist:
+                    adicionarMusicaAPlaylist(playlist, file_path)
+                    musicas_na_playlist.add(nome_musica)
     
-    return media_list
+    return playlist
 
-def pararMusica(player):
-    player.pause()
-
-def updateSlide(slide, player, duracao):
-    if player.get_media_player().get_media():
-        posicao_em_ms = player.get_media_player().get_time()
-    else:
-        posicao_em_ms = 0
-
-    if posicao_em_ms != -1:
-        posicao_em_segundos = posicao_em_ms
-        
-        if posicao_em_segundos != 0:
-            valor_do_slide = posicao_em_segundos / duracao * 100
-        else: 
-            valor_do_slide = 0
-        slide.setValue(int(valor_do_slide))
-    else:
-        print('A posição de reprodução não está disponível.')
-
-def updatePlayer(slide, player):
-    if player.get_media_player().get_media():
-        tamanhoMusica = ((player.get_media_player().get_media().get_duration()) / 1000)
-    else:
-        tamanhoMusica = 0
-
-    updateSlide(slide, player, tamanhoMusica * 1000)
+def pararMusica():
+    pygame.mixer.music.stop()
 
 def limpar_nome_musica(nome_musica):
-    # Decodificar URL
     nome_musica_decodificado = urllib.parse.unquote(nome_musica)
-    
-    # Remove a extensão ".mp3"
     nome_musica_sem_extensao = nome_musica_decodificado.rstrip('.mp3')
     
-    # Substitui caracteres especiais e espaços em branco por underscores
     nome_musica_formatado = re.sub(r'[^\w\s-]', '', nome_musica_sem_extensao)
     
     return nome_musica_formatado
-
-def updatePlaylist(media_list):
-    playlist = []
-
-    for idx in range(media_list.count()):
-        media = media_list.item_at_index(idx)
-        path = os.path.normpath(media.get_mrl())
-        file_name, file_extension = os.path.splitext(os.path.basename(path))
-        nome_musica = limpar_nome_musica(file_name)
-
-        info = {
-            "posicao": idx + 1,
-            "nome_musica": nome_musica
-        }
-        playlist.append(info)
-    
-    return playlist
